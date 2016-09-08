05beaf36ea74f21765bb1b8d7daf0a38ab0ee693
jona8690 <jona8690@edu.eal.dk>
2016-09-08 12:10:13 +0200

    Solution!!

--- 02_Bottles/Song.cs
+++ 02_Bottles/Song.cs
@@ -3,19 +3,27 @@ using System.Diagnostics;
 
 namespace _02_Bottles {
 	internal class Song {
+		private bool first = true;
+		private string bottle = "bottles";
 		internal string CountBottles(int v) {
 			string verse = "";
 			for(int i = v; i >= 0; i--) {
 				if(i == 0) {
 					verse = verse + " No more bottles of beer on the wall.";
-				} else if(i == 1) {
-					verse = verse + "1 bottle of beer on the wall."
-					+ " 1 bottle of beer."
-					+ " Take one down and pass it around.";
 				} else {
-					verse = verse + i + " bottles of beer on the wall. "
-				+ i + " bottles of beer."
-				+ " Take one down and pass it around. ";
+					if (i == 1) bottle = "bottle";
+
+					if(first) {
+						verse = i + " "+ bottle +" of beer on the wall. "
+							+ i + " " + bottle + " of beer."
+							+ " Take one down and pass it around.";
+						first = false;
+					} else {
+						verse += " " + i + " " + bottle + " of beer on the wall. "
+							+ i + " " + bottle + " of beer on the wall. "
+							+ i + " " + bottle + " of beer."
+							+ " Take one down and pass it around.";
+					}
 				}
 			}
 
