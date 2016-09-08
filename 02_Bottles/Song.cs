695e2c4befaea1f8630895bc57ccfc46f4a22b97
jona8690 <jona8690@edu.eal.dk>
2016-09-08 11:11:37 +0200

    Test two works...

--- 02_Bottles/Song.cs
+++ 02_Bottles/Song.cs
@@ -6,13 +6,12 @@ namespace _02_Bottles {
 		internal string CountBottles(int v) {
 			string verse = "";
 			for(int i = v; i >= 0; i--) {
-				Debug.WriteLine("i is currently: " + i);
 				if(i == 0) {
 					verse = verse + " No more bottles of beer on the wall.";
 				} else if(i == 1) {
 					verse = verse + "1 bottle of beer on the wall."
 					+ " 1 bottle of beer."
-					+ " Take one down and pass it around. ";
+					+ " Take one down and pass it around.";
 				} else {
 					verse = verse + i + " bottles of beer on the wall. "
 				+ i + " bottles of beer."
